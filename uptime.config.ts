bbba2f42af4b23060901728ef4686cd774cc9c75
DreadPirateRoberts <46958197+ManfredRichthofen@users.noreply.github.com>
2026-08-12 14:04:35 -0700

    Change status page title and monitor configuration
    Updated the title and configuration for the Jellyfin status page.

--- uptime.config.ts
+++ uptime.config.ts
@@ -7,7 +7,7 @@ import { MaintenanceConfig, PageConfig, WorkerConfig } from './types/config'
 
 const pageConfig: PageConfig = {
   // Title for your status page
-  title: "lyc8503's Status Page",
+  title: "Jellyfin Status Page",
   // Links shown at the header of your status page, could set `highlight` to `true`
   links: [
     // { link: 'https://github.com/lyc8503', label: 'GitHub' },
@@ -17,26 +17,12 @@ const pageConfig: PageConfig = {
 }
 
 const workerConfig: WorkerConfig = {
-  // Define all your monitors here
   monitors: [
-    // Example HTTP Monitor
     {
-      // `id` should be unique, history will be kept if the `id` remains constant
       id: 'jellyfin',
-      // `name` is used at status page and callback message
       name: 'Jellyfin',
-      // `method` should be a valid HTTP Method
       method: 'GET',
-      // `target` is a valid URL
-      target: 'https://jfapp.xyz/health',
-      // [OPTIONAL] `tooltip` is ONLY used at status page to show a tooltip
-      //tooltip: 'This is a tooltip for this monitor',
-      // [OPTIONAL] `statusPageLink` is ONLY used for clickable link at status page
-      //statusPageLink: 'https://example.com',
-      // [OPTIONAL] `expectedCodes` is an array of acceptable HTTP response codes, if not specified, default to 2xx
-      expectedCodes: [200],
-      // [OPTIONAL] `timeout` in millisecond, if not specified, default to 10000
-      timeout: 10000,
+      target: 'https://jfapp.xyz/health'
     },
         {
       id: 'dl_1',
